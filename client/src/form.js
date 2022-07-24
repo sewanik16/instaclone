fdf05077b7cf4df72020da3b7c6be1a38782a4a7
Nikhil <devsotnik@gmail.com>
2022-07-24 09:35:40 +0530

    after validation

--- client/src/form.js
+++ client/src/form.js
@@ -60,7 +60,7 @@ const Form = () => {
     <>
     <Header/>
       <div style={main}>
-        <form style={form}>
+        <form style={form} onSubmit={handleUserAdd}>
           <div style={input}>
             {/* <input type="text" id="filepath" name="filepath" value={path}  placeholder="No file chosen" style={{width:"280px",paddingLeft:"10px"}}/> */}
             {/* <input type="file" id="file" onChange={handlefile}  /> */}
@@ -69,23 +69,23 @@ const Form = () => {
               }}>
               Browse  
             </button> */}
-            
+          
             <div>
-            <input type="text" id="filepath" name="filepath" value={path.name}  placeholder="No file chosen" style={{width:"280px",paddingLeft:"10px"}}/>
+            <input required type="text" id="filepath" name="filepath" value={path.name}  placeholder="No file chosen" style={{width:"280px",paddingLeft:"10px"}}/>
               <label for="files" style={label}>Browse</label>
               <input id="files" hidden type="file" onChange={handlefile}/>
             </div>
          
           </div>
           <div style={input}>
-            <input id="name" name="name" onChange={handleInputs} placeholder="Author" maxLength={24} style={{width:"130px",paddingLeft:"10px"}}/>
-            <input id="location" name="location" onChange={handleInputs} placeholder="Location" maxLength={24} style={{width:"130px",paddingLeft:"10px"}}/>
+            <input required id="name" name="name" onChange={handleInputs} placeholder="Author" maxLength={24} style={{width:"130px",paddingLeft:"10px"}}/>
+            <input required id="location" name="location" onChange={handleInputs} placeholder="Location" maxLength={24} style={{width:"130px",paddingLeft:"10px"}}/>
           </div>
           <div style={input}>
-            <input id="description" name="description" onChange={handleInputs} placeholder="Description" maxLength={36} style={{width:"350px",paddingLeft:"10px"}}/>
+            <input required id="description" name="description" onChange={handleInputs} placeholder="Description" maxLength={36} style={{width:"350px",paddingLeft:"10px"}}/>
           </div>
           <div>
-            <button id="postbtn" type="submit" style={{padding:"2px 14px",color:"gray"}} onClick={handleUserAdd}>Post</button>
+            <button id="postbtn" type="submit" style={{padding:"2px 14px",color:"gray"}} >Post</button>
           </div>
         </form>
       </div>
